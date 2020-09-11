4e372815cc4add2dbfa1a262ec703b36ca9f7fbe
Batool Abbas <abbassbatoolali@gmail.com>
2020-09-12 02:53:56 +0500

    added Payment Gateway Mehod

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('');
+
+// API
+
+// - App config
+const app = express();
+
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+// - API routes
+app.get('/', (request, response) => response.status(200).send('hello world'))
+
+// app.get('/batool', (request, response) => response.status(200).send('batool'))
+
+app.post('/payments/create', async (request, response) => {
+    const total = request.query.total;
+
+    console.log('Payment Request Received BOOM!! FOR THIS AMOUNT >>>', total);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, //subunits of the currency
+        currency: "usd",
+    });
+
+    // OK - Created
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+// - Listen command
+exports.api = functions.https.onRequest(app)
+
+// Example endpoint
+// http://localhost:5001/clone-bt/us-central1/api
+
--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Login';
 import { useStateValue } from './StateProvider';
 import { auth } from './firebase';
+import Payment from './Payment';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import Orders from './Orders';
 
 function App() {
+
+  const promise = loadStripe('pk_test_51HQEFaARXwtU3QOW29tV3L8HBOEM2bLpnylqWVvsiP7CpqyCxT3TFUCoR825dlFu6G7YvFR8vVCViiq2l9KwpBMD00bzccUwKn');
 
   const [{}, dispatch] = useStateValue();
 
@@ -40,18 +46,36 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
 
           <Route path="/login">
             <Login />
           </Route>
+
           <Route path="/checkout" >
+            <Header />
             <Checkout />
           </Route>
+
+          <Route path="/payment" >
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
           
-          <Route path="/" >
+          <Route exact path="/orders" >
+            <Header />
+            <Orders />
+          </Route>
+
+          <Route exact path="/" >
+            <Header />
             <Home />
+          </Route>
+
+          <Route path="/*" >
+            <h1>Error no url found!!</h1>
           </Route>
 
         </Switch>
